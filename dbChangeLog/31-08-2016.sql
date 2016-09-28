f34dd2c2d8ffac06293aca90828761cc7fd83700
Vinícius Feitosa da Silva <viniciusfesil@gmail.com>
2016-09-28 10:36:57 -0300

    Update 31-08-2016.sql

---
+++ dbChangeLog/31-08-2016.sql
@@ -0,0 +1,4 @@
+/**
+ Alteração do tipo da coluna "dstextomanifestacao" da tabela "tbmanifestacao" para o tipo Text.
+ */
+ALTER TABLE tbmanifestacao ALTER COLUMN dstextomanifestacao text SET NOT NULL;
